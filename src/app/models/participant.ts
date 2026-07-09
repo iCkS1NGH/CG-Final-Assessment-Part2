068e57cf1628aac2c561d86f93923a7faae9ce28
iCkS1NGH <i.iconic.email@gmail.com>
2026-07-09 13:07:12 +0530

    add route feature

--- src/app/models/participant.ts
+++ src/app/models/participant.ts
@@ -1,4 +1,4 @@
-export interface Participant {
+export interface Participant{
   id: number;
   name: string;
   email: string;
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Participant } from '../models/participant';
+import {Injectable} from '@angular/core';
+import {Participant} from '../models/participant';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class EventService{
 
   private storageKey = 'participants';
 
-  getAll(): Participant[] {
+  getAll(): Participant[]{
     const data = localStorage.getItem(this.storageKey);
     return data?JSON.parse(data) : [];
   }
 
-  add(participant: Participant): void {
+  add(participant: Participant): void{
     const list = this.getAll();
     list.push(participant);
     localStorage.setItem(this.storageKey, JSON.stringify(list));
   }
 
-  delete(id: number): void {
+  delete(id: number): void{
     const list = this.getAll().filter(p => p.id !== id);
     localStorage.setItem(this.storageKey, JSON.stringify(list));
   }
